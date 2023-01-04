747f5db431e5ee9a66f400dde2a48434c22890e0
gabrielrangel95 <gabriel@sevenapps.com.br>
2023-01-04 12:08:33 -0500

    feat: 5 and 6 - Global Styles and Media Query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css'
 import { ThemeProvider } from 'styled-components'
 import { HomeScreen, ListScreen } from 'screens'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { theme } from 'styles'
+import { theme, GlobalStyle } from 'styles'
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Router>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
---
+++ src/styles/global.js
@@ -0,0 +1,69 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+  font-family: 'Avenir Next';
+  src: url('/public/fonts/AvenirNext-Regular.otf');
+  }
+
+  @font-face {
+    font-family: 'Avenir Next';
+    src: url('/public/fonts/AvenirNext-Bold.otf');
+    font-weight: bold;
+  }
+
+  @font-face {
+    font-family: 'Avenir Next';
+    src: url('/public/fonts/AvenirNext-It.otf');
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'Avenir Next';
+    src: url('/public/fonts/AvenirNext-Medium.ttf');
+    font-weight: 500;
+  }
+
+  body {
+    margin: 0;
+    font-family: 'Avenir Next';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+  }
+
+  h1 {
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 22px;
+    color: black;
+  }
+
+  h3 {
+    font-size: 16px;
+    font-weight: 500;
+    margin: 0px;
+    padding: 0px;
+  }
+
+  ::-webkit-scrollbar {
+    visibility: hidden;
+    width: 0px;
+  }
+
+  @media (max-width: 420px)  {
+    h1 {
+      font-size: 18px;
+    }
+
+    h3 {
+      font-size: 12px;
+    }
+  }
+
+
+`
